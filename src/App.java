6c3eb7b3dcd6a88a685ec2141f4bbff797cad4ae
Davi Santana <davisantana.ti@gmail.com>
2023-04-08 18:09:44 -0300

    Inicio da aplicacao java, criacao do ambiente

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+        
+        primaryStage.setTitle("DomBarbeiro");
+        primaryStage.setScene(tela);
+        primaryStage.show();        
+    }
+
+
+}

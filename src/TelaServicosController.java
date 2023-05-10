daaa1caba0e4302ded5f9d0c32f8a809e7c8bac8
Igor Sena <igorssena22@gmail.com>
2023-05-10 20:53:55 -0300

    Codigo com nomes dos inputs ja  settados

---
+++ src/TelaServicosController.java
@@ -0,0 +1,43 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class TelaServicosController {
+
+    @FXML
+    private Button botaoCadastrar;
+
+    @FXML
+    private Button botaoEditar;
+
+    @FXML
+    private Button botaoRemover;
+
+    @FXML
+    private TextField boxServicos;
+
+    @FXML
+    private TextField boxValor;
+
+    @FXML
+    private TableView<?> tabelaServico;
+
+    @FXML
+    void abrirTelaAdmin(MouseEvent event) {
+
+    }
+
+    @FXML
+    void abrirTelaAdminUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sairApp(ActionEvent event) {
+
+    }
+
+}
